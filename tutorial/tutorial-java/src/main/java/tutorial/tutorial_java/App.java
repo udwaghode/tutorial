542f57b232c3a35c91ca409d340f874fa8a36dd1
Umesh Waghode <umesh_waghode@persistent.co.in>
2019-02-06 18:52:24 +0530

    second commit on master

--- tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
+++ tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Hi there" );
+        System.out.println( "new version on master" );
     }
 }
