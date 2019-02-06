d009a4947499a98a1ba848c41c2bcb89241cccd4
Umesh Waghode <umesh_waghode@persistent.co.in>
2019-02-06 19:08:44 +0530

    second commit on branch

--- tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
+++ tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Hi there tutorialbrannch1" );
+        System.out.println( "Hello World!" );
     }
 }
