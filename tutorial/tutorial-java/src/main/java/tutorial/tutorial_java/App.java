dceabe6d39ce58b45f3f94ce9df772c7cd6322d4
Umesh Waghode <umesh_waghode@persistent.co.in>
2019-02-06 19:49:35 +0530

    new change

--- tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
+++ tutorial/tutorial-java/src/main/java/tutorial/tutorial_java/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! Hi there" );
         System.out.println( "new version on master" );
+        System.out.println( "new change" );
     }
 }
